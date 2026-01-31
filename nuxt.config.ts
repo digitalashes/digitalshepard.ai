9719626ac2e0e9dff099f6e02ec1c01696b7f412
Shepard <digitalashes@users.noreply.github.com>
2026-01-31 21:04:06 +0100

    Add initial configuration and dependencies

---
+++ nuxt.config.ts
@@ -0,0 +1,63 @@
+import { definePerson } from "nuxt-schema-org/schema";
+import appMeta from "./app/app.meta";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: "2024-04-03",
+    ssr: false,
+    ogImage: { enabled: false },
+    devtools: { enabled: true },
+    app: {
+        head: {
+            htmlAttrs: { lang: "en" },
+            title: appMeta.name,
+            link: [
+                { rel: "alternate", type: "application/rss+xml", title: "Digital Shepard RSS", href: "/feed.xml" },
+            ],
+            script: [
+                {
+                    src: "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX",
+                    async: true,
+                },
+                {
+                    innerHTML: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-XXXXXXXXXX');",
+                },
+            ],
+        },
+    },
+    css: ["~/assets/css/main.css"],
+    fonts: {
+        defaults: {
+            weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        },
+    },
+    site: {
+        name: appMeta.name,
+        url: appMeta.url,
+        defaultLocale: "en",
+    },
+    schemaOrg: {
+        identity: definePerson(appMeta.author),
+    },
+    content: {
+        build: {
+            markdown: {
+                toc: {
+                    depth: 3,
+                    searchDepth: 2,
+                },
+            },
+        },
+    },
+    nitro: {
+        prerender: {
+            routes: ["/feed.xml"],
+        },
+    },
+    modules: [
+        "@nuxt/ui",
+        "@nuxt/content",
+        "@nuxtjs/seo",
+        "@nuxt/eslint",
+    ],
+});
